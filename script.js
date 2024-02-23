5de2cc7ae54e5b77bf0810afa0c7b09e734fd0fc
Roman <dervoedroman05@gmail.com>
2024-02-23 19:26:01 +0300

    final update stage1

--- script.js
+++ script.js
@@ -2,11 +2,15 @@ let numberOfFilms = +prompt("Сколько фильмов вы смотрели
 
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: '',
-    actors: '',
+    movies: {},
+    actors: {},
     genres: [],
     privat: false
-}
+};
 
 let lastFilmWatching = prompt("Один из последних просмотренных фильмов", "");
-let grade = prompt("На сколько его оцените", "")
+let grade = prompt("На сколько его оцените", "");
+
+personalMovieDB.movies[lastFilmWatching] = grade;
+
+console.log(personalMovieDB);
