e7d9a4ce8eb5c0d0cd24c00ab06742fa1c80c8ca
Roman <dervoedroman05@gmail.com>
2024-02-24 14:47:21 +0300

    final update stage 2

--- script.js
+++ script.js
@@ -8,9 +8,25 @@ let personalMovieDB = {
     privat: false
 };
 
-let lastFilmWatching = prompt("Один из последних просмотренных фильмов", "");
-let grade = prompt("На сколько его оцените", "");
+if(personalMovieDB.count < 10){
+    alert("Просмотрено довольно мало фильмов");
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert("Вы классический зритель");
+} else if(personalMovieDB.count > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
-personalMovieDB.movies[lastFilmWatching] = grade;
+for( let i = 0; i < 2; i++){
+
+    let lastFilmWatching = prompt("Один из последних просмотренных фильмов", "");
+    let grade = prompt("На сколько его оцените", "");
+    if( lastFilmWatching == ' ' || grade === ' ' || lastFilmWatching === null || grade === null || lastFilmWatching.length > 50){
+        i--;
+    } else {
+        personalMovieDB.movies[lastFilmWatching] = grade;
+    }
+}
 
 console.log(personalMovieDB);
